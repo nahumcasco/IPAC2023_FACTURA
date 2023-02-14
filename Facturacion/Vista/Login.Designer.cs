c0b535783b7f3cad1884b89db9f4362f5f04d8da
Nahum Casco <nahumcasco@gmail.com>
2023-02-13 20:03:29 -0600

    Login
    Login

---
+++ Facturacion/Vista/Login.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Vista
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CodigoUsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.ImagenPictureBox = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // CodigoUsuarioTextBox
+            // 
+            this.CodigoUsuarioTextBox.Location = new System.Drawing.Point(88, 26);
+            this.CodigoUsuarioTextBox.Name = "CodigoUsuarioTextBox";
+            this.CodigoUsuarioTextBox.Size = new System.Drawing.Size(217, 22);
+            this.CodigoUsuarioTextBox.TabIndex = 2;
+            // 
+            // ContraseñaTextBox
+            // 
+            this.ContraseñaTextBox.Location = new System.Drawing.Point(88, 64);
+            this.ContraseñaTextBox.Name = "ContraseñaTextBox";
+            this.ContraseñaTextBox.PasswordChar = '*';
+            this.ContraseñaTextBox.Size = new System.Drawing.Size(217, 22);
+            this.ContraseñaTextBox.TabIndex = 3;
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelarButton.Image = global::Vista.Properties.Resources.cancelar;
+            this.CancelarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CancelarButton.Location = new System.Drawing.Point(202, 98);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(103, 41);
+            this.CancelarButton.TabIndex = 6;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AceptarButton.Image = global::Vista.Properties.Resources.aceptar;
+            this.AceptarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AceptarButton.Location = new System.Drawing.Point(88, 98);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(108, 41);
+            this.AceptarButton.TabIndex = 5;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            this.AceptarButton.Click += new System.EventHandler(this.AceptarButton_Click);
+            // 
+            // ImagenPictureBox
+            // 
+            this.ImagenPictureBox.BackColor = System.Drawing.Color.White;
+            this.ImagenPictureBox.Image = global::Vista.Properties.Resources.user;
+            this.ImagenPictureBox.Location = new System.Drawing.Point(345, 35);
+            this.ImagenPictureBox.Name = "ImagenPictureBox";
+            this.ImagenPictureBox.Size = new System.Drawing.Size(109, 104);
+            this.ImagenPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagenPictureBox.TabIndex = 4;
+            this.ImagenPictureBox.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.AceptarButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelarButton;
+            this.ClientSize = new System.Drawing.Size(466, 169);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.ImagenPictureBox);
+            this.Controls.Add(this.ContraseñaTextBox);
+            this.Controls.Add(this.CodigoUsuarioTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Activated += new System.EventHandler(this.Login_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CodigoUsuarioTextBox;
+        private System.Windows.Forms.TextBox ContraseñaTextBox;
+        private System.Windows.Forms.PictureBox ImagenPictureBox;
+        private System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Facturacion/Vista/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Activated(object sender, EventArgs e)
+        {
+            CodigoUsuarioTextBox.Focus();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (CodigoUsuarioTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(CodigoUsuarioTextBox, "Ingrese un usuario");
+                return;
+            }
+            errorProvider1.Clear();
+            if (ContraseñaTextBox.Text == "")
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una contraseña");
+                return;
+            }
+            errorProvider1.Clear();
+        }
+    }
+}
